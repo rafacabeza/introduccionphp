0e4f4596e844f6bc5ea7bfce1dbb7a03d4808d3e
rafacabeza <rafacabeza@gmail.com>
2016-05-06 20:16:27 +0200

    framework 01 v1

--- framework/01/app/Bootstrap.php
+++ framework/01/app/Bootstrap.php
@@ -18,11 +18,21 @@ class Bootstrap
         $controller = $_GET['controller'];
         $method = $_GET['method'];
         
-        echo 'Estamos sen Bootstrap, el controlador frontal, raíz de la' 
-                . 'apliación de ahora en adelante. <br>' ;
+        echo 'Estamos en Bootstrap, el controlador frontal. <br>';
+        echo ' Esta será la raíz de la apliación de ahora en adelante. <br>' ;
         echo 'Esto es el __construct, algo asi como el main. <br>';
         
-        echo "Vamos a ejecutar $controller-->$method";
+        echo '<hr>';
+        if(empty($controller)){
+            echo 'Observa la URL. Despues pulsa el siguiente enlace y vuelve a '
+            . 'mirar<br>';
+            echo '<a href="?controller=usuario&method=nuevo"> Alta de usuario </a>';
+        }
+        else{
+            echo "Vamos a ejecutar $controller-->$method";
+            
+        }
+           
     }
 
 }
--- views/mvc/01.php
+++ views/mvc/01.php
@@ -1,4 +1,9 @@
 <div id="main">
-    v01
+    <h1>Modelo vista controlador</h1>
+    <h2>El controlador frontal</h2>
+    
+    
+    <a href="<?php echo Config::HOST;?>framework/01/">Enlace</a>
+    
 </div><div>
     
